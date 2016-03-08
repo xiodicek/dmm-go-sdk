b5d004da4f22621ec9c4cc632ac5d176b02bde34
Yusuke Komatsu <tienlen042@gmail.com>
2016-03-08 21:15:50 +0900

    add dummyProductService

--- api/product_test.go
+++ api/product_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestNewProductService(t *testing.T) {
-    affiliate_id := "foobar-999"
-    api_id       := "TXpEZ5D4T2xB3J5cuSLf"
+    affiliate_id := Dummy_Affliate_Id
+    api_id       := Dummy_Api_Id
 
     srv := NewProductService(affiliate_id, api_id)
     if srv.AffiliateId != affiliate_id {
@@ -16,4 +16,8 @@ func TestNewProductService(t *testing.T) {
     if srv.ApiId != api_id {
         t.Fatalf("ProductService.ApiId is expected to equal the input value(api_id)")
     }
+}
+
+func dummyProductService() *ProductService {
+    return NewProductService(Dummy_Affliate_Id, Dummy_Api_Id)
 }
