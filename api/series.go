dc1c72df58e5f6e9ad833c3dd4bc0f4791ff03af
Yusuke Komatsu <tienlen042@gmail.com>
2016-03-09 16:47:21 +0900

    add functions in series.go and series_test.go

--- api/series.go
+++ api/series.go
@@ -1,13 +1,144 @@
 package api
 
+import (
+    "fmt"
+    "net/url"
+    "strconv"
+    "github.com/mitchellh/mapstructure"
+)
+
 type SeriesService struct {
-    ApiId        string
-    AffiliateId  string
+    ApiId        string `mapstructure:"api_id"`
+    AffiliateId  string `mapstructure:"affiliate_id"`
+    FloorId      string `mapstructure:"floor_id"`
+    Initial      string `mapstructure:"initial"`
+    Length       int64  `mapstructure:"hits"`
+    Offset       int64  `mapstructure:"offset"`
 }
 
 func NewSeriesService(affiliateId, apiId string) *SeriesService {
     return &SeriesService{
         ApiId:       apiId,
         AffiliateId: affiliateId,
+        FloorId:     "",
+        Initial:     "",
+        Length:      DEFAULT_API_LENGTH,
+        Offset:      DEFAULT_API_OFFSET,
     }
+}
+
+type SeriesRawResponse struct {
+    Request SeriesService  `mapstructure:"request"`
+    Result  SeriesResponse `mapstructure:"result"`
+}
+
+type SeriesResponse struct {
+    ResultCount   int64    `mapstructure:"result_count"`
+    TotalCount    int64    `mapstructure:"total_count"`
+    FirstPosition int64    `mapstructure:"first_position"`
+    SiteName      string   `mapstructure:"site_name"`
+    SiteCode      string   `mapstructure:"site_code"`
+    ServiceName   string   `mapstructure:"service_name"`
+    ServiceCode   string   `mapstructure:"service_code"`
+    FloorId       string   `mapstructure:"floor_id"`
+    FloorName     string   `mapstructure:"floor_name"`
+    FloorCode     string   `mapstructure:"floor_code"`
+    SeriesList    []Series `mapstructure:"series"`
+}
+
+type Series struct {
+    SeriesId string `mapstructure:"series_id"`
+    Name     string `mapstructure:"name"`
+    Ruby     string `mapstructure:"ruby"`
+    ListURL  string `mapstructure:"list_url"`
+}
+
+func (srv *SeriesService) Execute() (*SeriesResponse, error) {
+    result, err := srv.ExecuteWeak()
+    if err != nil {
+        return nil, err
+    }
+    var raw SeriesRawResponse
+    if err = mapstructure.WeakDecode(result, &raw); err != nil {
+        return nil, err
+    }
+    return &raw.Result, nil
+}
+
+func (srv *SeriesService) ExecuteWeak() (interface{}, error) {
+    reqUrl, err := srv.BuildRequestUrl()
+    if err != nil {
+        return nil, err
+    }
+
+    return RequestJson(reqUrl)
+}
+
+func (srv *SeriesService) SetLength(length int64) *SeriesService {
+    srv.Length = length
+    return srv
+}
+
+func (srv *SeriesService) SetHits(length int64) *SeriesService {
+    srv.SetLength(length)
+    return srv
+}
+
+func (srv *SeriesService) SetOffset(offset int64) *SeriesService {
+    srv.Offset = offset
+    return srv
+}
+
+func (srv *SeriesService) SetInitial(initial string) *SeriesService {
+    srv.Initial = TrimString(initial)
+    return srv
+}
+
+func (srv *SeriesService) SetFloorId(floor_id string) *SeriesService {
+    srv.FloorId = TrimString(floor_id)
+    return srv
+}
+
+func (srv *SeriesService) ValidateLength() bool {
+    return ValidateRange(srv.Length, 1, DEFAULT_MAX_LENGTH)
+}
+
+func (srv *SeriesService) ValidateOffset() bool {
+    return srv.Offset >= 1
+}
+
+func (srv *SeriesService) BuildRequestUrl() (string, error) {
+    if srv.ApiId == "" {
+        return "", fmt.Errorf("set invalid ApiId parameter.")
+    }
+    if !ValidateAffiliateId(srv.AffiliateId) {
+        return "", fmt.Errorf("set invalid AffiliateId parameter.")
+    }
+    if srv.FloorId == "" {
+        return "", fmt.Errorf("set invalid FloorId parameter.")
+    }
+
+    queries := url.Values{}
+    queries.Set("api_id", srv.ApiId)
+    queries.Set("affiliate_id", srv.AffiliateId)
+    queries.Set("floor_id", srv.FloorId)
+
+    if srv.Length != 0 {
+        if !srv.ValidateLength() {
+            return "", fmt.Errorf("length out of range: %d", srv.Length)
+        }
+        queries.Set("hits", strconv.FormatInt(srv.Length, 10))
+    }
+
+    if srv.Offset != 0 {
+        if !srv.ValidateOffset() {
+            return "", fmt.Errorf("offset out of range: %d", srv.Offset)
+        }
+        queries.Set("offset", strconv.FormatInt(srv.Offset, 10))
+    }
+
+    if (srv.Initial != "") {
+        queries.Set("initial", srv.Initial)
+    }
+    return API_BASE_URL + "/SeriesSearch?" + queries.Encode(), nil
 }
---
+++ api/series_test.go
@@ -0,0 +1,224 @@
+package api
+
+import (
+    "testing"
+    "net/url"
+    "strconv"
+)
+
+func TestNewSeriesService(t *testing.T) {
+    affiliate_id := Dummy_Affliate_Id
+    api_id       := Dummy_Api_Id
+
+    srv := NewSeriesService(affiliate_id, api_id)
+    if srv.AffiliateId != affiliate_id {
+        t.Fatalf("SeriesService.AffiliateId is expected to equal the input value(affiliate_id)")
+    }
+
+    if srv.ApiId != api_id {
+        t.Fatalf("SeriesService.ApiId is expected to equal the input value(api_id)")
+    }
+}
+
+func TestSetLengthInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+    var length int64 = 10
+    srv.SetLength(length)
+
+    if srv.Length != length {
+        t.Fatalf("SeriesService.Length is expected to equal the input value(length)")
+    }
+}
+
+func TestSetHitsInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+    var hits int64 = 10
+    srv.SetHits(hits)
+
+    if srv.Length != hits {
+        t.Fatalf("SeriesService.Length is expected to equal the input value(hits)")
+    }
+}
+
+func TestSetOffsetInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+    var offset int64 = 10
+    srv.SetOffset(offset)
+
+    if srv.Offset != offset {
+        t.Fatalf("SeriesService.Offset is expected to equal the input value(offset)")
+    }
+}
+
+func TestValidateLengthInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+
+    var target int64
+
+    target = 1
+    srv.SetLength(target)
+    if srv.ValidateLength() == false {
+        t.Fatalf("SeriesService.ValidateLength is expected TRUE.")
+    }
+
+    target = DEFAULT_API_LENGTH
+    srv.SetLength(target)
+    if srv.ValidateLength() == false {
+        t.Fatalf("SeriesService.ValidateLength is expected TRUE.")
+    }
+
+    target = DEFAULT_MAX_LENGTH
+    srv.SetLength(target)
+    if srv.ValidateLength() == false {
+        t.Fatalf("SeriesService.ValidateLength is expected TRUE.")
+    }
+
+    target = DEFAULT_MAX_LENGTH + 1
+    srv.SetLength(target)
+    if srv.ValidateLength() == true {
+        t.Fatalf("SeriesService.ValidateLength is expected FALSE.")
+    }
+
+    target = 0
+    srv.SetLength(target)
+    if srv.ValidateLength() == true {
+        t.Fatalf("SeriesService.ValidateLength is expected FALSE.")
+    }
+
+    target = -1
+    srv.SetLength(target)
+    if srv.ValidateLength() == true {
+        t.Fatalf("SeriesService.ValidateLength is expected FALSE.")
+    }
+}
+
+func TestValidateOffsetInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+
+    var target int64
+
+    target = 1
+    srv.SetOffset(target)
+    if srv.ValidateOffset() == false {
+        t.Fatalf("SeriesService.ValidateOffset is expected TRUE.")
+    }
+
+    target = 100
+    srv.SetOffset(target)
+    if srv.ValidateOffset() == false {
+        t.Fatalf("SeriesService.ValidateOffset is expected TRUE.")
+    }
+
+    target = 0
+    srv.SetOffset(target)
+    if srv.ValidateOffset() == true {
+        t.Fatalf("SeriesService.ValidateOffset is expected FALSE.")
+    }
+
+    target = -1
+    srv.SetOffset(target)
+    if srv.ValidateOffset() == true {
+        t.Fatalf("SeriesService.ValidateOffset is expected FALSE.")
+    }
+}
+
+func TestBuildRequestUrlInSeriesService(t *testing.T) {
+    var srv *SeriesService
+    var u string
+    var err error
+    var expected string
+
+    srv = dummySeriesService()
+    srv.SetFloorId("40")
+    u, err = srv.BuildRequestUrl()
+    expected = API_BASE_URL + "/SeriesSearch?affiliate_id=" + Dummy_Affliate_Id + "&api_id=" + Dummy_Api_Id + "&floor_id=40" + "&hits=" + strconv.FormatInt(DEFAULT_API_LENGTH, 10) + "&offset=" + strconv.FormatInt(DEFAULT_API_OFFSET, 10)
+    if u != expected {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to equal the expected value.\nexpected:%s\nactual:  %s", expected, u)
+    }
+    if err != nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is not expected to have error")
+    }
+
+    srv = dummySeriesService()
+    srv.SetLength(0)
+    srv.SetOffset(0)
+    srv.SetFloorId("40")
+    u, err = srv.BuildRequestUrl()
+    expected = API_BASE_URL + "/SeriesSearch?affiliate_id=" + Dummy_Affliate_Id + "&api_id=" + Dummy_Api_Id + "&floor_id=40"
+    expected_base := expected
+    if u != expected {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to equal the expected value.\nexpected:%s\nactual:  %s", expected, u)
+    }
+    if err != nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is not expected to have error")
+    }
+
+    srv.SetLength(-1)
+    u, err = srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected empty if error occurs.")
+    }
+    if err == nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to return error.")
+    }
+    srv.SetLength(0)
+
+    srv.SetOffset(-1)
+    u, err = srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected empty if error occurs.")
+    }
+    if err == nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to return error.")
+    }
+    srv.SetOffset(0)
+
+    srv.SetFloorId("")
+    u, err = srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected empty if error occurs.")
+    }
+    if err == nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to return error.")
+    }
+    srv.SetFloorId("40")
+
+    srv.SetInitial("あ")
+    expected = expected_base + "&initial=" + url.QueryEscape("あ")
+    u, err = srv.BuildRequestUrl()
+    if u != expected {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to equal the expected value.\nexpected:%s\nactual:  %s", expected, u)
+    }
+    if err != nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is not expected to have error")
+    }
+    srv.SetInitial("")
+}
+
+func TestBuildRequestUrlWithoutApiIdInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+    srv.ApiId = ""
+    u, err := srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected empty if API ID is not set.")
+    }
+    if err == nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to return error.")
+    }
+}
+
+func TestBuildRequestUrlWithWrongAffiliateIdInSeriesService(t *testing.T) {
+    srv := dummySeriesService()
+    srv.AffiliateId = "fizzbizz-100"
+    u, err := srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected empty if wrong Affiliate ID is set.")
+    }
+    if err == nil {
+        t.Fatalf("SeriesService.BuildRequestUrl is expected to return error.")
+    }
+}
+
+func dummySeriesService() *SeriesService {
+    return NewSeriesService(Dummy_Affliate_Id, Dummy_Api_Id)
+}
