d884d0b50512ad360ff7683d92d46792f12eda04
Yusuke Komatsu <tienlen042@gmail.com>
2016-03-09 16:40:41 +0900

    add floor's test

--- api/floor_test.go
+++ api/floor_test.go
@@ -17,3 +17,48 @@ func TestNewFloorService(t *testing.T) {
         t.Fatalf("FloorService.ApiId is expected to equal the input value(api_id)")
     }
 }
+
+func TestBuildRequestUrlInFloorService(t *testing.T) {
+    var srv *FloorService
+    var u string
+    var err error
+    var expected string
+
+    srv = dummyFloorService()
+    u, err = srv.BuildRequestUrl()
+    expected = API_BASE_URL + "/FloorList?affiliate_id=" + Dummy_Affliate_Id + "&api_id=" + Dummy_Api_Id
+    if u != expected {
+        t.Fatalf("FloorService.BuildRequestUrl is expected to equal the expected value.\nexpected:%s\nactual:  %s", expected, u)
+    }
+    if err != nil {
+        t.Fatalf("FloorService.BuildRequestUrl is not expected to have error")
+    }
+}
+
+func TestBuildRequestUrlWithoutApiIdInInFloorService(t *testing.T) {
+    srv := dummyFloorService()
+    srv.ApiId = ""
+    u, err := srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("FloorService.BuildRequestUrl is expected empty if API ID is not set.")
+    }
+    if err == nil {
+        t.Fatalf("FloorService.BuildRequestUrl is expected to return error.")
+    }
+}
+
+func TestBuildRequestUrlWithWrongAffiliateIdInFloorService(t *testing.T) {
+    srv := dummyFloorService()
+    srv.AffiliateId = "fizzbizz-100"
+    u, err := srv.BuildRequestUrl()
+    if u != "" {
+        t.Fatalf("FloorService.BuildRequestUrl is expected empty if wrong Affiliate ID is set.")
+    }
+    if err == nil {
+        t.Fatalf("FloorService.BuildRequestUrl is expected to return error.")
+    }
+}
+
+func dummyFloorService() *FloorService {
+    return NewFloorService(Dummy_Affliate_Id, Dummy_Api_Id)
+}
