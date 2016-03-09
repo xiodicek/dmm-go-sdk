11fdec48ee5b44cbe3d8740a545b261760b0c469
Yusuke Komatsu <tienlen042@gmail.com>
2016-03-09 18:29:52 +0900

    add functions in author and author test

--- api/author.go
+++ api/author.go
@@ -1,13 +1,144 @@
 package api
 
+import (
+    "fmt"
+    "net/url"
+    "strconv"
+    "github.com/mitchellh/mapstructure"
+)
+
 type AuthorService struct {
-    ApiId        string
-    AffiliateId  string
+    ApiId        string `mapstructure:"api_id"`
+    AffiliateId  string `mapstructure:"affiliate_id"`
+    FloorId      string `mapstructure:"floor_id"`
+    Initial      string `mapstructure:"initial"`
+    Length       int64  `mapstructure:"hits"`
+    Offset       int64  `mapstructure:"offset"`
+}
+
+type AuthorRawResponse struct {
+    Request AuthorService  `mapstructure:"request"`
+    Result  AuthorResponse `mapstructure:"result"`
+}
+
+type AuthorResponse struct {
+    ResultCount   int64    `mapstructure:"result_count"`
+    TotalCount    int64    `mapstructure:"total_count"`
+    FirstPosition int64    `mapstructure:"first_position"`
+    SiteName      string   `mapstructure:"site_name"`
+    SiteCode      string   `mapstructure:"site_code"`
+    ServiceName   string   `mapstructure:"service_name"`
+    ServiceCode   string   `mapstructure:"service_code"`
+    FloorId       string   `mapstructure:"floor_id"`
+    FloorName     string   `mapstructure:"floor_name"`
+    FloorCode     string   `mapstructure:"floor_code"`
+    AuthorList    []Author `mapstructure:"author"`
+}
+
+type Author struct {
+    AuthorId  string `mapstructure:"author_id"`
+    Name      string `mapstructure:"name"`
+    Ruby      string `mapstructure:"ruby"`
+    ListURL   string `mapstructure:"list_url"`
 }
 
 func NewAuthorService(affiliateId, apiId string) *AuthorService {
     return &AuthorService{
         ApiId:       apiId,
         AffiliateId: affiliateId,
+        FloorId:     "",
+        Initial:     "",
+        Length:      DEFAULT_API_LENGTH,
+        Offset:      DEFAULT_API_OFFSET,
     }
+}
+
+func (srv *AuthorService) Execute() (*AuthorResponse, error) {
+    result, err := srv.ExecuteWeak()
+    if err != nil {
+        return nil, err
+    }
+    var raw AuthorRawResponse
+    if err = mapstructure.WeakDecode(result, &raw); err != nil {
+        return nil, err
+    }
+    return &raw.Result, nil
+}
+
+func (srv *AuthorService) ExecuteWeak() (interface{}, error) {
+    reqUrl, err := srv.BuildRequestUrl()
+    if err != nil {
+        return nil, err
+    }
+
+    return RequestJson(reqUrl)
+}
+
+func (srv *AuthorService) SetLength(length int64) *AuthorService {
+    srv.Length = length
+    return srv
+}
+
+func (srv *AuthorService) SetHits(length int64) *AuthorService {
+    srv.SetLength(length)
+    return srv
+}
+
+func (srv *AuthorService) SetOffset(offset int64) *AuthorService {
+    srv.Offset = offset
+    return srv
+}
+
+func (srv *AuthorService) SetInitial(initial string) *AuthorService {
+    srv.Initial = TrimString(initial)
+    return srv
+}
+
+func (srv *AuthorService) SetFloorId(floor_id string) *AuthorService {
+    srv.FloorId = TrimString(floor_id)
+    return srv
+}
+
+func (srv *AuthorService) ValidateLength() bool {
+    return ValidateRange(srv.Length, 1, DEFAULT_MAX_LENGTH)
+}
+
+func (srv *AuthorService) ValidateOffset() bool {
+    return srv.Offset >= 1
+}
+
+func (srv *AuthorService) BuildRequestUrl() (string, error) {
+    if srv.ApiId == "" {
+        return "", fmt.Errorf("set invalid ApiId parameter.")
+    }
+    if !ValidateAffiliateId(srv.AffiliateId) {
+        return "", fmt.Errorf("set invalid AffiliateId parameter.")
+    }
+    if srv.FloorId == "" {
+        return "", fmt.Errorf("set invalid FloorId parameter.")
+    }
+
+    queries := url.Values{}
+    queries.Set("api_id", srv.ApiId)
+    queries.Set("affiliate_id", srv.AffiliateId)
+    queries.Set("floor_id", srv.FloorId)
+
+    if srv.Length != 0 {
+        if !srv.ValidateLength() {
+            return "", fmt.Errorf("length out of range: %d", srv.Length)
+        }
+        queries.Set("hits", strconv.FormatInt(srv.Length, 10))
+    }
+
+    if srv.Offset != 0 {
+        if !srv.ValidateOffset() {
+            return "", fmt.Errorf("offset out of range: %d", srv.Offset)
+        }
+        queries.Set("offset", strconv.FormatInt(srv.Offset, 10))
+    }
+
+    if (srv.Initial != "") {
+        queries.Set("initial", srv.Initial)
+    }
+    return API_BASE_URL + "/AuthorSearch?" + queries.Encode(), nil
 }
